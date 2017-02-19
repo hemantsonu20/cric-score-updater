1ab5bb1ef0e8d71c5e1de3fb12c8c60920574300
hemant <hemantsonu20@gmail.com>
2017-02-19 20:23:40 +0530

    unused import

--- cric-lib/src/main/java/com/github/cric/common/service/cripapi/CricApiScoreService.java
+++ cric-lib/src/main/java/com/github/cric/common/service/cripapi/CricApiScoreService.java
@@ -1,7 +1,6 @@
 package com.github.cric.common.service.cripapi;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
